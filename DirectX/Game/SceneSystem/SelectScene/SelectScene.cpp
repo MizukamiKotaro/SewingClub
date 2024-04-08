3b92a9a6c2687587a9a10db2d8e3ccadae2a104f
Taka <k022g0068@g.neec.ac.jp>
2024-04-08 18:02:29 +0900

    セレクトシーンに仮でいろいろ追加

--- DirectX/Game/SceneSystem/SelectScene/SelectScene.cpp
+++ DirectX/Game/SceneSystem/SelectScene/SelectScene.cpp
@@ -1,34 +1,94 @@
 #include "SelectScene.h"
 #include "Kyoko.h"
+#include "Ease/Ease.h"
+
+#include<imgui.h>
 
 SelectScene::SelectScene()
 {
 	FirstInit();
 
-	
+	//オブジェクト初期化
+	for (int i = 0; i < _countOfStageNumbers; i++) {
+		stageBoxes_[i] = std::make_unique<Model>("WaterCircle");
+
+		stageBoxes_[i]->Initialize();
+	}
+
+
 }
 
 void SelectScene::Initialize()
 {
+	//カメラ初期化
+	camera_->Initialize();
 
+	//ステージ箱選択
+	float diff = 8;
+	int count = 0;
+	for (auto& box : stageBoxes_) {
+		//初期化と配置
+		box->Initialize();
+		box->transform_.translate_.x = -diff + diff * count;
+		box->Update();
+		count++;
+	}
+
+	for (auto& t : ts_) {
+		t = 0;
+	}
+
+	//データ初期化
+	switchData_.changeReception = true;
+	switchData_.moveLowerLimit = 0.8f;
+	switchData_.resetFlagLimit = 0.2f;
+	switchData_.maxSelectBoxScale_ = 1.5f;
+	switchData_.minSelectBoxScale_ = 1.0f;
 }
 
 void SelectScene::Update()
 {
+	//デバッグ
+	Debug();
+
+	//カメラ更新
+	camera_->Update();
+
+
+	//ステージを選ぶ処理
+	SelectStage();
+
+	//シーン変更関係処理
 	SceneChange();
 }
 
 void SelectScene::Draw()
 {
-	
+
 	//必須
 	Kyoko::Engine::PreDraw();
 
-	
-	BlackDraw();
+	//ステージ選択BOX
+	for (auto& box : stageBoxes_) {
+		box->Draw(*camera_.get());
+	}
 
+	//シーン転換時のフェードインアウト
+	BlackDraw();
 	//必須
 	Kyoko::Engine::PostDraw();
+}
+
+
+void SelectScene::Debug()
+{
+#ifdef _DEBUG
+	ImGui::Begin("debug");
+	ImGui::Text("flag : %d , pickedNum : %d", switchData_.changeReception,pickedNum_);
+	ImGui::End();
+#endif // _DEBUG
+
+
 }
 
 void SelectScene::SceneChange()
@@ -37,6 +97,79 @@ void SelectScene::SceneChange()
 		// シーン切り替え
 		stageNo_ = 0;
 		ChangeScene(STAGE);
+	}
+}
+
+void SelectScene::SelectStage()
+{
+
+#pragma region 入力による選ばれている番号変更処理
+
+	Vector2 lStick = input_->GetGamePadLStick();
+
+	//操作受付フラグONの時
+	if (switchData_.changeReception) {
+		//入力をみて変更
+		if (lStick.x >= switchData_.moveLowerLimit) {
+			pickedNum_++;
+			switchData_.changeReception = false;			
+		}
+		if (lStick.x <= -switchData_.moveLowerLimit) {
+			pickedNum_--;
+			switchData_.changeReception = false;
+		}
+
+		//選択外なら戻す
+		if (pickedNum_ >= _countOfStageNumbers) {
+			pickedNum_ = _countOfStageNumbers - 1;
+		}
+		else if (pickedNum_ < 0) {
+			pickedNum_ = 0;
+		}
+	}
+	else {
+		//値がリセット範囲内の時
+		if (lStick.x <= switchData_.resetFlagLimit && lStick.x >= -switchData_.resetFlagLimit) {
+			switchData_.changeReception = true;
+		}
+	}
+
+
+	
+		
+	//各更新処理
+	for (int i = 0; i < _countOfStageNumbers; i++) {
+		if (i == pickedNum_) {
+
+			ts_[i] += switchData_.maxScaleMinute;
+			if (ts_[i] > 1.0f) {
+				ts_[i] = 1.0f;
+			}
+		}
+		else {
+			ts_[i] -= switchData_.minScaleMinute;
+			if (ts_[i] < 0.0f) {
+				ts_[i] = 0.0f;
+			}
+		}
+
+		float scale = Ease::UseEase(switchData_.minSelectBoxScale_, switchData_.maxSelectBoxScale_, ts_[i]);
+
+		Vector3 scaleV3 = { scale,scale, scale };
+
+		//拡大
+		stageBoxes_[i]->transform_.scale_ = scaleV3;
+	}
+
+#pragma endregion
+
+
+
+
+
+	//ステージ箱の更新
+	for (auto& box : stageBoxes_) {
+		box->Update();
 	}
 }
 
--- DirectX/Game/SceneSystem/SelectScene/SelectScene.h
+++ DirectX/Game/SceneSystem/SelectScene/SelectScene.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "SceneSystem/IScene/IScene.h"
-
+#include "Model.h"
 
 class SelectScene : public IScene
 {
@@ -13,12 +13,49 @@ public:
 	void Draw() override;
 
 private:
+	//デバッグ
+	void Debug();
 
 	//シーン変更処理
 	void SceneChange();
 
+	//ステージを選ぶ処理
+	void SelectStage();
+
 private:
-	
+
+	//ステージ管理
+	enum StageNumbers {
+		ST1,
+		ST2,
+		ST3,
+		_countOfStageNumbers	//数
+	};
+
+	//選択するステージボックス
+	std::unique_ptr<Model>stageBoxes_[_countOfStageNumbers];
+
+	//各ステージの配置データを持っておくところが欲しい
+
+	//選ばれている番号
+	int32_t pickedNum_ = 0;
+
+	float ts_[_countOfStageNumbers];
+
+	//切り替えをするときの変数軍
+	struct  inputAcceptance {
+		bool changeReception;	//操作を受け付けるかフラグ
+		float moveLowerLimit;	//移動する際の最低量必須のスティック量
+		float resetFlagLimit;	//フラグをリセットするための最低限必須な量	
+		float maxSelectBoxScale_ = 1.5f;//選ばれているセレクトの最大スケール		
+		float minSelectBoxScale_ = 1.0f;//最小スケール
+		float maxScaleMinute = 1.0f / 30.0f;
+		float minScaleMinute = 1.0f / 10.0f;
+	};
+
+	inputAcceptance switchData_;
+
+
 
 
 };
