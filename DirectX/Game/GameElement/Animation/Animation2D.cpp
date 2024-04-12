00758734a3009a4b84790ea2e6bda113505070f1
hamu4792game <112112847+hamu4792game@users.noreply.github.com>
2024-04-12 14:20:03 +0900

    左上0から右上8、右下64にかけて番号で保存するよう実装

--- DirectX/Game/GameElement/Animation/Animation2D.cpp
+++ DirectX/Game/GameElement/Animation/Animation2D.cpp
@@ -5,7 +5,7 @@ void Animation2D::Initialize(Model* model, const float& height, const float& wid
 	model_ = model;
 	texParam_.textureSize = Vector2(width, height);
 	texParam_.divisionNumber = Vector2(static_cast<float>(wDivNum), static_cast<float>(hDivNum));
-	texParam_.sceneSize = Vector2(static_cast<float>(wDivNum) / width, static_cast<float>(hDivNum) / height);
+	texParam_.sceneSize = Vector2(width / static_cast<float>(wDivNum), height / static_cast<float>(hDivNum));
 
 	uvScale_ = Vector3(1.0f / static_cast<float>(wDivNum), 1.0f / static_cast<float>(hDivNum),1.0f);
 
@@ -17,20 +17,23 @@ void Animation2D::Initialize(Model* model, const float& height, const float& wid
 void Animation2D::Update() {
 	if (Input::GetInstance()->PressedKey(DIK_SPACE)) {
 		nowScene_ += 1u;
-		if (nowScene_ >= sceneNumberList_.size()) { nowScene_ = 0u; }
+		if (nowScene_ >= sceneNumberList_.size()) { 
+			nowScene_ = 0u;
+		}
 	}
 	model_->SetUVParam(uvScale_, Vector3(0.0f, 0.0f, 0.0f), sceneNumberList_.at(nowScene_));
 }
 
 void Animation2D::SceneEntry() {
+	uint32_t handle = 0u;
 	for (uint32_t y = 0u; y < texParam_.divisionNumber.y; y++) {
 		for (uint32_t x = 0u; x < texParam_.divisionNumber.x; x++) {
 			Vector3 pos{};
-			pos.x = static_cast<float>(x) * texParam_.sceneSize.x;
-			pos.y = static_cast<float>(y) * texParam_.sceneSize.y;
+			pos.x = static_cast<float>(x) * uvScale_.x;
+			pos.y = static_cast<float>(y) * uvScale_.y;
 			pos.z = 0.0f;
-			uint32_t handle = x + y;
 			sceneNumberList_[handle] = pos;
+			handle++;
 		}
 	}
 }
