2c16cd3d8893c9a7b0564c7d085f76ce8275031a
Taka <k022g0068@g.neec.ac.jp>
2024-05-14 17:25:12 +0900

    BGM用を用意

--- DirectX/Game/GameElement/OptionUI/OptionUI.cpp
+++ DirectX/Game/GameElement/OptionUI/OptionUI.cpp
@@ -5,14 +5,19 @@ OptionUI::OptionUI()
 {
 	input_ = Input::GetInstance();
 
+	VM_ = VolumeManager::GetInstance();
+
 	backSprite_ = std::make_unique<Sprite>("white.png");
 	backSprite_->size_ = { 1280,720 };
 	blackFrame_ = std::make_unique<Sprite>("title_option_whitecloud.png");
 
-	spSoundGageFrame_ = std::make_unique<Sprite>("soundGageFrame.png");
-	spSoundGageBar_ = std::make_unique<Sprite>("soundGageBar.png");
-	spSoundText_ = std::make_unique<Sprite>("soundGageText.png");
-	spSoundGageNum_ = std::make_unique<Sprite>("soundGageFrameNum.png");
+	for (int i = 0; i < _countOption; i++) {
+		spSoundGageFrame_[i] = std::make_unique<Sprite>("soundGageFrame.png");
+		spSoundGageBar_[i] = std::make_unique<Sprite>("soundGageBar.png");
+		spSoundText_[i] = std::make_unique<Sprite>("soundGageText.png");
+		spSoundGageNum_[i] = std::make_unique<Sprite>("soundGageFrameNum.png");
+	}
+
 }
 
 OptionUI::~OptionUI()
@@ -22,17 +27,26 @@ OptionUI::~OptionUI()
 void OptionUI::Initialize()
 {
 	backSprite_->SetColor({ 0, 0, 0, backalpha_ });
-	
+
 #pragma region 音関係
-	
-	//一戸となりにずらす
-	spSoundText_->pos_.x -= spSoundText_->size_.x * 1.0f;
-	spSoundGageNum_->pos_.y -= spSoundGageNum_->size_.y * 1.0f;
+	for (int i = 0; i < _countOption; i++) {
 
-	spSoundGageBar_->SetAnchorPoint({ 0.0f,0.5f });
-	spSoundGageBar_->pos_.x -= spSoundGageBar_->size_.x/2;
+		spSoundGageFrame_[i]->pos_.y += 100.0f * i;
+		spSoundGageBar_[i]->pos_.y += 100.0f * i;
+		spSoundText_[i]->pos_.y += 100.0f * i;
+		spSoundGageNum_[i]->pos_.y += 100.0f * i;
 
-	gageBarMaxScale_ = spSoundGageBar_->size_.x;
+		//一戸となりにずらす
+		spSoundText_[i]->pos_.x -= spSoundText_[i]->size_.x * 1.0f;
+		spSoundGageNum_[i]->pos_.y -= spSoundGageNum_[i]->size_.y * 1.0f;
+
+		spSoundGageBar_[i]->SetAnchorPoint({ 0.0f,0.5f });
+		spSoundGageBar_[i]->pos_.x -= spSoundGageBar_[i]->size_.x / 2;
+
+		gageBarMaxScale_[i] = spSoundGageBar_[i]->size_.x;
+
+
+	}
 #pragma endregion
 
 }
@@ -45,9 +59,10 @@ bool OptionUI::Update()
 
 	AudioBarUpdate();
 
-	
+
 
 	if (input_->PressedGamePadButton(Input::GamePadButton::START)) {
+
 		return  false;
 	}
 	return true;
@@ -59,22 +74,65 @@ void OptionUI::Draw()
 	backSprite_->Draw();
 	blackFrame_->Draw();
 
-	spSoundGageFrame_->Draw();
-	spSoundGageBar_->Draw();
-	spSoundText_->Draw();
-	spSoundGageNum_->Draw();
+
+
+	for (int i = 0; i < _countOption; i++) {
+		//更新
+		spSoundGageFrame_[i]->Draw();
+		spSoundGageBar_[i]->Draw();
+		spSoundText_[i]->Draw();
+		spSoundGageNum_[i]->Draw();
+	}
 }
 
 void OptionUI::AudioBarUpdate()
 {
 
-	//volumeの値からサイズを変更
-	float newScaleX = Calc::Lerp(0, gageBarMaxScale_, volume_);
-	spSoundGageBar_->size_.x = newScaleX;
+	if (nowSelect == BGMVolume) {
+		volume_[nowSelect] = VM_->GetMusicVolumeStage();
+	}
+	else if (nowSelect == SEVolume) {
+		volume_[nowSelect] = VM_->GetSEVolumeStage();
+	}
+	
+	
 
-	//更新
-	spSoundGageFrame_->Update();
-	spSoundGageBar_->Update();
-	spSoundText_->Update();
-	spSoundGageNum_->Update();
+	//入力で音量変更
+	Vector2 input = input_->GetGamePadLStick();
+
+	
+
+
+
+	if (input.x > 0) {
+		volume_[nowSelect] += moveValue_;
+	}
+	else if (input.x < 0) {
+		volume_[nowSelect] -= moveValue_;
+	}
+	if (volume_[nowSelect] < 0.0f) {
+		volume_[nowSelect] = 0.0f;
+	}
+	else if (volume_[nowSelect] > 1.0f) {
+		volume_[nowSelect] = 1.0f;
+	}
+
+	//volumeの値からサイズを変更
+	float newScaleX = Calc::Lerp(0, gageBarMaxScale_[nowSelect], volume_[nowSelect]);
+	spSoundGageBar_[nowSelect]->size_.x = newScaleX;
+
+	if (nowSelect == BGMVolume) {
+		VM_->SetMusicVolumeStage(volume_[nowSelect]);
+	}
+	else if (nowSelect == SEVolume) {
+		VM_->SetSEVolumeStage(volume_[nowSelect]);
+	}
+
+	for (int i= 0; i < _countOption; i++) {
+		//更新
+		spSoundGageFrame_[i]->Update();
+		spSoundGageBar_[i]->Update();
+		spSoundText_[i]->Update();
+		spSoundGageNum_[i]->Update();
+	}
 }
--- DirectX/Game/GameElement/OptionUI/OptionUI.h
+++ DirectX/Game/GameElement/OptionUI/OptionUI.h
@@ -1,7 +1,7 @@
 #pragma once
 #include"Input.h"
 #include"Sprite.h"
-
+#include"Audio/VolumeManager/VolumeManager.h"
 #include<iostream>
 
 //ポーズ画面の演出
@@ -24,6 +24,7 @@ private:
 private:
 
 	Input* input_;
+	VolumeManager* VM_;
 
 	//背景スプライト
 	std::unique_ptr<Sprite>backSprite_;
@@ -35,21 +36,34 @@ private:
 
 
 #pragma region 音設定関係
+
+	enum OptionItem {
+		BGMVolume,
+		SEVolume,
+		_countOption
+	};
+
+	OptionItem nowSelect = BGMVolume;
+
 	//画像群
 	//ゲージ枠
-	std::unique_ptr<Sprite>spSoundGageFrame_;
+	std::unique_ptr<Sprite>spSoundGageFrame_[_countOption];
 	//ゲージバー
-	std::unique_ptr<Sprite>spSoundGageBar_;
+	std::unique_ptr<Sprite>spSoundGageBar_[_countOption];
 	//テキスト（音量
-	std::unique_ptr<Sprite>spSoundText_;
+	std::unique_ptr<Sprite>spSoundText_[_countOption];
 	//ゲージ上の数字
-	std::unique_ptr<Sprite>spSoundGageNum_;
+	std::unique_ptr<Sprite>spSoundGageNum_[_countOption];
+
 
 	//ゲージの元サイズ保存
-	float gageBarMaxScale_ = 0;
+	float gageBarMaxScale_[_countOption] = { 0 };
 
 	//音量(ここを変えてバー変更)
-	float volume_ = 0.5f;
+	float volume_[_countOption] = { 0.5f };
+
+	//入力で変わる音量
+	float moveValue_ = 0.01f;
 #pragma endregion
 
 };
