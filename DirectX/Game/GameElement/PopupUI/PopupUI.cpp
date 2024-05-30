5b5f2467cbd0fe6407f1a62eb1efc0f5af44a6f1
hamu4792game <112112847+hamu4792game@users.noreply.github.com>
2024-05-30 12:40:59 +0900

    popupUIのバグなおしたのに治ってないの喃々直しましたバーカ

--- DirectX/Game/GameElement/PopupUI/PopupUI.cpp
+++ DirectX/Game/GameElement/PopupUI/PopupUI.cpp
@@ -1,5 +1,6 @@
 #include "PopupUI.h"
 #include "Utils/Ease/Ease.h"
+#include <algorithm>
 
 PopupUI::PopupUI() {
 	sprite_ = std::make_unique<Sprite>();
@@ -12,7 +13,7 @@ void PopupUI::Initialize() {
 	isFinished_ = false;
 	alpha_ = 1.0f;
 	scale_ = Vector2(0.0f, 0.0f);
-	sprite_->pos_.y += 640.0f;
+	sprite_->pos_.y = 500.0f;
 	SpriteUpdate();
 }
 
@@ -47,6 +48,7 @@ void PopupUI::SpriteUpdate() {
 }
 
 void PopupUI::PopUp() {
+	counter_.nowFrame = std::clamp(counter_.nowFrame, 0.0f, counter_.kMaxPopFrame);
 	scale_ = Ease::UseEase(Vector2(0.0f,0.0f), kMaxSize_, counter_.nowFrame, counter_.kMaxPopFrame, Ease::EaseType::EaseOut, 4);
 	if (counter_.nowFrame >= counter_.kMaxPopFrame) {
 		phase = Phase::Wait;
@@ -55,6 +57,7 @@ void PopupUI::PopUp() {
 }
 
 void PopupUI::Wait() {
+	counter_.nowFrame = std::clamp(counter_.nowFrame, 0.0f, counter_.kMaxWaitFrame);
 	if (counter_.nowFrame >= counter_.kMaxWaitFrame) {
 		phase = Phase::FadeOut;
 		counter_.Initialize();
@@ -62,6 +65,7 @@ void PopupUI::Wait() {
 }
 
 void PopupUI::FadeOut() {
+	counter_.nowFrame = std::clamp(counter_.nowFrame, 0.0f, counter_.kMaxFadeFrame);
 	alpha_ = Ease::UseEase(1.0f, 0.0f, counter_.nowFrame, counter_.kMaxFadeFrame, Ease::EaseType::EaseOut, 3);
 	if (counter_.nowFrame >= counter_.kMaxFadeFrame) {
 		phase = Phase::PopUp;
